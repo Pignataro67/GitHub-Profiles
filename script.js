eed1ec462b2d74cf070c7f3258cfb2c065439791
Pignataro67 <Pignataro67@gmail.com>
2021-09-30 04:57:34 -0700

    adding JS

---
+++ script.js
@@ -0,0 +1,13 @@
+const APIURL = 'https://api.github.com/users/'
+
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+async function getUser(username) {
+  try {
+    const { data } = await axios(APIURL + username)
+  } catch (err) {
+      
+  }
+}
