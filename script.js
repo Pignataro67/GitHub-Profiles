32e3fce810206fa6140f61b2f460858ebc02467d
Pignataro67 <Pignataro67@gmail.com>
2021-10-03 05:31:46 -0700

    adding JS

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ const search = document.getElementById('search')
 async function getUser(username) {
   try {
     const { data } = await axios(APIURL + username)
+
+    createUserCard(data)
+    getRepos(username)
   } catch (err) {
-      
+    if(err.response.status == 404) {
+      createErrorCard('No profile with this username') 
   }
 }
